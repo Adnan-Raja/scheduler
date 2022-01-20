daa6899fae1453bef3ee0d58473e98ab40784989
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-01-19 20:22:55 -0500

    InterviewerList.js is made values are extracted from props and used

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+import "components/InterviewerList.scss";
+
+import InterviewerListItem from './InterviewerListItem';
+
+
+
+export default function InterviewerList(props) {
+  // const interviewersItem = classNames('interviewers__item', {
+  //   'interviewers__item--selected': props.selected
+  // });
+ 
+  const interviewerObj = props.interviewers.map((list)=> {
+  return(
+    <InterviewerListItem 
+    key={list.id}
+    name={list.name}
+    avatar={list.avatar}
+    setInterviewer={props.setInterviewer}
+    selected={list.id === props.interviewer}
+    />
+  )
+  });
+ 
+
+  return <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">{interviewerObj}</ul>
+  </section>
+};
