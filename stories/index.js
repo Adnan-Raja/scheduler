7c74a923930f8846f2ee93dfafb1d8205c778a4d
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-01-20 15:25:20 -0500

    feat:more components (Confirm, Error, Show and Status) are made in Appointement directory

--- stories/index.js
+++ stories/index.js
@@ -11,15 +11,23 @@ import DayListItem from "components/DayListItem";
 
 import DayList from "components/DayList";
 
-import InterviewerList from "components/InterviewerList"
+import InterviewerList from "components/InterviewerList";
 
-import InterviewerListItem from "components/InterviewerListItem"
+import InterviewerListItem from "components/InterviewerListItem";
 
 import Appointment from "components/Appointment/index.js";
 
-import Header from "components/Appointment/Header"
+import Header from "components/Appointment/Header";
 
-import Empty from "components/Appointment/Empty.js"
+import Empty from "components/Appointment/Empty.js";
+
+import Show from "components/Appointment/Show";
+
+import Confirm from "components/Appointment/Confirm";
+
+import Status from "components/Appointment/Status";
+
+import Error from "components/Appointment/Error";
 
 
 
@@ -156,3 +164,11 @@ storiesOf("Button", module)
       .add("Appointment with Time", () => <Appointment time="12pm" />)
       .add("Header", () => <Header time="12pm" />)
       .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+      .add("Show", () => <Show 
+        student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}
+      />)
+      .add("Confirm", () => <Confirm 
+        message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}
+      />)
+      .add("Status", () => <Status message="Deleting" />)
+      .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
