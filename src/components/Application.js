28cb8cab965b3ed29d9535741a2eac4c0531256e
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-01-21 01:30:42 -0500

    feat: all components are updated and are working in story book. Application.js component is also updated to show on webpack-dev-server environment.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,58 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 
+import Appointment from "./Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+const listItems = appointments.map((appointment) => {
+  return (
+    // Array of DayListItem 
+    <Appointment 
+    key={appointment.id}
+    id={appointment.id}
+    time={appointment.time}
+    interview={appointment.interview} />
+  )
+});
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
 
@@ -45,18 +97,11 @@ export default function Application(props) {
       />
       </section>
        <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {listItems}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
 
-
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
 
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,9 +2,22 @@ import React from "react";
 
 import "./styles.scss";
 
+import Header from "./Header";
+
+import Show from "./Show";
+
+import Empty from "./Empty";
+
 export default function Appointment(props) {
   
   return (
-    <article className="appointment">{props.time ? "Appointement at " + props.time : "No Appointement" }</article>
+    <main>
+      {/* <article className="appointment">{props.time ?  props.time : "No Appointement" }</article> */}
+       <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {/* {props.time === "5pm" ? <Header className="appointment :last-of-type __add"/> : props.time } */}
+    </main>
+    
   );
 }
+
