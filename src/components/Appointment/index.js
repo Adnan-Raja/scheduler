264f62aa66d8d7383129a4ce56dd8f5a38d1bee9
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-01-23 21:36:36 -0500

    feat: getAppointmentsForDay is implemented and tested using unit test

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,11 +11,9 @@ import Empty from "./Empty";
 export default function Appointment(props) {
   
   return (
-    <main>
-      {/* <article className="appointment">{props.time ?  props.time : "No Appointement" }</article> */}
-       <Header time={props.time} />
+    <main className="appointment">
+      <Header time={props.time} />
       {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
-      {/* {props.time === "5pm" ? <Header className="appointment :last-of-type __add"/> : props.time } */}
     </main>
     
   );
---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+
+
+export function getAppointmentsForDay(state, dayName) {
+
+  const filteredAppointments = state.days.filter(day=> day.name=== dayName).map(d=> d.appointments).flat();
+
+  const appointments = [];
+
+  for(const appointmentId of filteredAppointments ){
+    if(state.appointments[appointmentId]){
+      appointments.push(state.appointments[appointmentId]);
+    }
+  }
+  return appointments;
+}
